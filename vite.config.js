9ff16f6f85a2d33ab76447a8323ba05e7f19681d
Gustavo Souza <gustavoelia7@gmail.com>
2024-08-02 09:11:29 -0300

    setup output directory on the vite config file

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,9 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/'
+    base: '/',
+    build: {
+        outDir: './build',
+        emptyOutDir: true,
+    }
 })
