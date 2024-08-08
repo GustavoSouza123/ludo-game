ba577dca3453e4ff0c80b063edc127ca03964307
Gustavo Souza <gustavoelia7@gmail.com>
2024-08-08 10:08:57 -0300

    update vite config for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/',
+    base: '/ludo-game/',
     build: {
         outDir: './build',
         emptyOutDir: true,
-    }
-})
+    },
+});
